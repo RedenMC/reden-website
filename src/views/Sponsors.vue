26990f2e4a5e402d3e6ecc6cb0fe955ffff1a659
Liyan Zhao <return65535@qq.com>
2023-11-05 18:58:52 +0800

    sponsors

--- src/views/Sponsors.vue
+++ src/views/Sponsors.vue
@@ -1,16 +1,41 @@
-<script setup>
+<script setup lang="ts">
 
+</script>
+
+<script lang="ts">
+import axios from 'axios'
+
+export default {
+  name: "Sponsors",
+  data() {
+    axios.get('/api/sponsors').then((res) => {
+      this.sponsors = res.data
+    })
+    return {
+      sponsors: []
+    }
+  }
+}
 </script>
 
 <template>
-
+  <v-list
+    subheader
+    three-line
+    v-for="sponsor in sponsors"
+  >
+    <v-list-item
+      :key="sponsor.name"
+    >
+      <v-list-item-title class="text-h6">{{ sponsor.name }}</v-list-item-title>
+      <v-list-item-subtitle>{{ sponsor.description }}</v-list-item-subtitle>
+      <v-list-item-action>
+        <v-img :src="sponsor.avatar"/>
+      </v-list-item-action>
+    </v-list-item>
+  </v-list>
 </template>
 
-<script>
-export default {
-  name: "Sponsors"
-}
-</script>
 
 <style scoped>
 
