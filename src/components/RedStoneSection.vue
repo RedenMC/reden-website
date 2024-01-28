b8d78ea77980db97c3bf1e8b0e28357b4e4fb370
YuQuuu <25228940@qq.com>
2024-01-28 17:29:45 +0800

    add RedStoneSection
    add RedStoneSection Components

---
+++ src/components/RedStoneSection.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import {onMounted, type Ref, ref} from "vue";
+
+const { size } = defineProps({
+  size: Number,
+});
+if (size === undefined || size <= 1) {
+  throw new Error('size must be greater than 0')
+}
+const sizz = Math.floor(size);
+
+let lightLine: Ref<Element | null> = ref(null);
+onMounted(() => {
+  let observer = new IntersectionObserver((it) => {
+    it.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+      } else {
+        entry.target.classList.remove('animate');
+      }
+    })
+  })
+  observer.observe(lightLine.value as Element);
+})
+</script>
+
+<template>
+  <div class="line">
+
+    <div class="darkLine lineLayout">
+      <div class="photo r0" v-for="n in sizz" :key="n"><slot :name="n"></slot></div>
+    </div>
+
+    <div class="lightLine lineLayout" ref="lightLine">
+      <div class="photo r15" v-for="n in sizz" :key="n"></div>
+    </div>
+
+    <slot></slot>
+
+  </div>
+</template>
+
+<style scoped>
+.photo {
+  height: 100px;
+  image-rendering: pixelated;
+}
+.r0 {
+  background: url("@/assets/redstone_dust_0.png") no-repeat;
+  background-size: 100px 100px;
+}
+.r15 {
+  background: url("@/assets/redstone_dust_15.png") no-repeat;
+  background-size: 100px 100px;
+}
+
+.lineLayout {
+  display: flex;
+  flex-direction: column;
+}
+
+.line {
+  position: relative;
+}
+
+.lightLine {
+  top: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  mask: linear-gradient(var(--c0) 0%, var(--c1) 10%);
+
+}
+
+.animate {
+  animation: mask 1s ease-out alternate;
+}
+
+@property --c0 {
+  syntax: '<color>';
+  inherits: false;
+  initial-value: #fff;
+}
+@property --c1 {
+  syntax: '<color>';
+  inherits: false;
+  initial-value: #fff;
+}
+
+@keyframes mask {
+  0% {
+    --c0: #fff;
+    --c1: transparent;
+  }
+  100% {
+    --c0: #fff;
+    --c1: #fff;
+  }
+}
+
+</style>
