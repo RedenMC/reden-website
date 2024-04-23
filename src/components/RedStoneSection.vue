8316e975f1e53eb2950e2170f7b803c95f5038ee
忠陽 李 <SmallSun698@gmail.com>
2024-04-23 23:11:16 +0800

    fix

--- src/components/RedStoneSection.vue
+++ src/components/RedStoneSection.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, type Ref, ref, VueElement } from 'vue';
+import { onMounted, type Ref, ref } from 'vue';
 
 // todo: 不透明度渐变
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
